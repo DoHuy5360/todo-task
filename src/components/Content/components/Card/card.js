99f47c1b283f8a70ff651dade5d8fe8f60003d9c
Dohuy5360 <loldh321@gmail.com>
2022-12-24 14:52:16 +0700

    Clone card display

--- src/components/Content/components/Card/card.js
+++ src/components/Content/components/Card/card.js
@@ -4,16 +4,30 @@ import { GrAttachment } from "react-icons/gr";
 import { FaRegUserCircle } from "react-icons/fa";
 function Card({ title, description, id }) {
 	const card = useRef();
+	const cardClone = useRef();
 	function handleDragStart(e) {
 		e.dataTransfer.setData("Text", e.target.id);
 		console.log(`Dragged: %c${title}`, "color: green; font-weight: bold");
+		cardClone.current = card.current.cloneNode(true);
+		card.current.parentNode.appendChild(cardClone.current);
 		card.current.classList.add("opacity-0");
 	}
+	function handleDrag(e) {
+		Object.assign(cardClone.current.style, {
+			position: "fixed",
+			top: `${e.pageY}px`,
+			left: `${e.pageX}px`,
+			boxShadow: "0 0.125rem 0.25rem #939393",
+			zIndex: 1,
+			transform: "translate(-100%, -100%)",
+		});
+	}
 	function handleDragEnd() {
+		cardClone.current.remove();
 		card.current.classList.remove("opacity-0");
 	}
 	return (
-		<div id={id} ref={card} onDragStart={handleDragStart} onDragEnd={handleDragEnd} draggable className="card relative flex flex-col gap-2 py-2 pl-2 pr-4 rounded bg-white cursor-grab">
+		<div id={id} ref={card} onDragStart={handleDragStart} onDragEnd={handleDragEnd} onDrag={handleDrag} draggable className="card relative flex flex-col gap-2 py-2 pl-2 pr-4 rounded bg-white cursor-grab">
 			<div className="flex items-center justify-between gap-2">
 				<div className="flex items-center">
 					<div>

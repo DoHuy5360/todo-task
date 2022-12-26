bb8dfc0f8b62c2055bd4b04a20e6a697f9cf3fe1
Dohuy5360 <loldh321@gmail.com>
2022-12-26 16:42:11 +0700

    Use dnd successfully!

--- src/components/Content/components/StatusColumn/statusColumn.js
+++ src/components/Content/components/StatusColumn/statusColumn.js
@@ -1,57 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useDrop } from "react-dnd";
 import { AiOutlinePlus } from "react-icons/ai";
-import Card from "../Card/Card";
+import { listCards } from "../../content";
 
-const listcard = [
-	[
-		{ title: "123a", description: "Lorem" },
-		{ title: "123b", description: "Lorem" },
-		{ title: "123c", description: "Lorem" },
-		{ title: "123d", description: "Lorem" },
-		{ title: "123e", description: "Lorem" },
-	],
-	[
-		{ title: "456a", description: "Lorem" },
-		{ title: "456b", description: "Lorem" },
-		{ title: "456c", description: "Lorem" },
-	],
-	[{ title: "789a", description: "Lorem" }],
-];
+function StatusColumn({ name, amountCard, colId, onRefresh, children }) {
+	const [{ isOver }, drop] = useDrop({
+		accept: "card",
+		drop: (item, monitor) => {
+			listCards[item.index].status = colId;
+			onRefresh((pre) => {
+				return pre ? false : true;
+			});
+		},
+		collect: (monitor) => ({
+			isOver: monitor.isOver(),
+		}),
+	});
 
-function StatusColumn({ name, amountCard, id }) {
-	const column = useRef();
-	function handleDragOver(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		column.current.classList.add("bg-slate-200");
-	}
-	function handleDragLeave(e) {
-		column.current.classList.remove("bg-slate-200");
-	}
-	function handleDrop(e) {
-		console.log(`Droped to: %c${name}`, "color: blue; font-weight: bold");
-		column.current.classList.remove("bg-slate-200");
-		const cardId = e.dataTransfer.getData("Text");
-		const cardSelected = document.getElementById(cardId);
-		const cardNodes = column.current.querySelectorAll(".card");
-		for (let idx = 0; idx < cardNodes.length; idx++) {
-			console.log("loop");
-			const card = cardNodes[idx];
-			const cardIndex = card.getBoundingClientRect();
-			const cardOrder = e.clientY - cardIndex.top - cardIndex.height / 2;
-			if (cardOrder < 0) {
-				column.current.insertBefore(cardSelected, card);
-				// column.current.insertBefore(cardClone, card);K
-				console.log("insert before card: " + idx);
-			} else {
-				column.current.appendChild(cardSelected);
-				// column.current.appendChild(cardClone);K
-				console.log("insert after -> " + idx);
-			}
-			break;
-		}
-		// column.current.appendChild(cardSelected);
-	}
 	return (
 		<div className="flex flex-col gap-2 bg-slate-300 w-full p-2 rounded h-[560px] pb-4 overflow-hidden">
 			<div className="select-none">
@@ -63,18 +27,8 @@ function StatusColumn({ name, amountCard, id }) {
 					<AiOutlinePlus />
 				</div>
 			</div>
-			<div
-				ref={column}
-				onDragOver={handleDragOver}
-				onDragLeave={handleDragLeave}
-				onDrop={handleDrop}
-				className="dropField flex flex-col gap-2 overflow-y-scroll h-full
-				 hover:scrollbar-thumb-slate-400 scrollbar-track-transparent scrollbar-thin
-				scrollbar-thumb-rounded-full scrollbar-track-rounded-full duration-100 ease-linear"
-			>
-				{listcard[id].map((card, idx) => (
-					<Card title={card.title} description={card.description} key={"card-" + id + "-" + idx} id={"card-" + id + "-" + idx}></Card>
-				))}
+			<div id={colId} ref={drop} className={(isOver ? "bg-slate-200" : "bg-transparent") + " dropField flex flex-col gap-2 overflow-y-scroll h-full hover:scrollbar-thumb-slate-400 scrollbar-track-transparent scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full duration-100 ease-linear"}>
+				{children}
 			</div>
 		</div>
 	);
--- src/components/Content/content.js
+++ src/components/Content/content.js
@@ -1,28 +1,52 @@
+import { DndProvider } from "react-dnd";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import StatusColumn from "./components/StatusColumn/StatusColumn.js";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Card from "./components/Card/Card.js";
+import { useState } from "react";
 const listColumns = [
 	{ name: "Todo", amountCard: 32 },
 	{ name: "In Progress", amountCard: 452 },
 	{ name: "Completed", amountCard: 1 },
 ];
+export const listCards = [
+	{ id: 0, status: 0, title: "123a", description: "Lorem" },
+	{ id: 1, status: 0, title: "123b", description: "Lorem" },
+	{ id: 2, status: 0, title: "123c", description: "Lorem" },
+	{ id: 3, status: 0, title: "123d", description: "Lorem" },
+	{ id: 4, status: 0, title: "123e", description: "Lorem" },
+	{ id: 5, status: 1, title: "456a", description: "Lorem" },
+	{ id: 6, status: 1, title: "456b", description: "Lorem" },
+	{ id: 7, status: 1, title: "456c", description: "Lorem" },
+	{ id: 8, status: 2, title: "789a", description: "Lorem" },
+];
 function Content() {
+	const [refresh, setRefresh] = useState(false);
 	return (
-		<div className="overflow-hidden">
-			<div className="flex items-baseline justify-between py-8">
-				<div className="text-2xl">Title</div>
-				<div className="flex items-center gap-2 cursor-pointer">
-					<div>This Week</div>
-					<div>
-						<MdKeyboardArrowDown />
+		<DndProvider backend={HTML5Backend}>
+			<div className="overflow-hidden">
+				<div className="flex items-baseline justify-between py-8">
+					<div className="text-2xl">Title</div>
+					<div className="flex items-center gap-2 cursor-pointer">
+						<div>This Week</div>
+						<div>
+							<MdKeyboardArrowDown />
+						</div>
 					</div>
 				</div>
+				<div className="flex gap-4 h-full">
+					{listColumns.map((column, colidx) => (
+						<StatusColumn name={column.name} amountCard={column.amountCard} key={"column-" + colidx} colId={colidx} onRefresh={setRefresh}>
+							{listCards.map((card, idx) => {
+								if (card.status === colidx) {
+									return <Card title={card.title} description={card.description} key={"card-" + colidx + "-" + idx} id={"card-" + colidx + "-" + idx} status={card.status} cardId={card.id} index={idx}></Card>;
+								}
+							})}
+						</StatusColumn>
+					))}
+				</div>
 			</div>
-			<div className="flex gap-4 h-full">
-				{listColumns.map((column, idx) => (
-					<StatusColumn name={column.name} amountCard={column.amountCard} key={"column-" + idx} id={idx}></StatusColumn>
-				))}
-			</div>
-		</div>
+		</DndProvider>
 	);
 }
 
